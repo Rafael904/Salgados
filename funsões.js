b9d2e98abbf17a9d788fbe8e6d2a707f31b7cbe8
Rafael904 <1849rafael@gmail.com>
2024-12-13 09:07:03 -0300

    Festa no copo salgados

---
+++ funsões.js
@@ -0,0 +1,10 @@
+var f = document.getElementById('duzentos')
+function menu() {
+    f.innerHTML = `Menu principal`
+}
+function sair() {
+    f.innerHTML = `O melhor salgado da <br/>cidade`
+}
+function cardapio() {
+    f.innerHTML = `Cardápio`
+}
